7370c1c9999399b8422db0b1bb2cc772adbb7c22
ManulPrian <156845683+ManulPrian@users.noreply.github.com>
2024-04-04 15:41:28 +0000

    Pending changes exported from your codespace

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,24 @@
 import React from "react";
+import ReactDom from "react-dom";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+function contador(props) {
+	return(
+		<div className="calendario">
+			<i className="far fa-clock"></i>
+			<div className="four">{Math.floor(props.digitFour % 10)}</div>
+			<div className="three">{Math.floor(props.digitThree % 10)}</div>
+			<div className="two">{Math.floor(props.digitTwo % 10)}</div>
+			<div className="one">{Math.floor(props.digitOne % 10)}</div>
 		</div>
 	);
+}
+
+contador.propTypes = {
+	digitFour: Proptypes.number,
+	digitThree: Proptypes.number,
+	digitTwo: Proptypes.number,
+	digitOne: Proptypes.number
 };
 
-export default Home;
+export default contador;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,19 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+import Contador from "./component/home";
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function ()  {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor((counter / 100) % 10);
+    const two = Math.floor((counter / 10) % 10);
+    const one = Math.floor(counter % 10);
+    console.log(four, three, two, one);
+    ReactDom.render(
+        <contador digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
+        document.getElementById("app")
+    );
+    counter++;    
+}, 1000);
